cbbc313144b74342a746e9ad6961732cb69084ef
Mauro Rossi <issor.oruam@gmail.com>
2018-08-10 01:12:22 +0200

    Modify color inversion matrix to swap Red and Blue colors
    Instead of color inversion it will produce Red and Blue colors swap,
    in order to correct displayed colors for R4xx and other old drivers
    still based on KMS API

--- services/accessibility/java/com/android/server/accessibility/DisplayAdjustmentUtils.java
+++ services/accessibility/java/com/android/server/accessibility/DisplayAdjustmentUtils.java
@@ -50,10 +50,10 @@ class DisplayAdjustmentUtils {
      * for full implementation details.
      */
     private static final float[] MATRIX_INVERT_COLOR = new float[] {
-        0.402f, -0.598f, -0.599f, 0,
-       -1.174f, -0.174f, -1.175f, 0,
-       -0.228f, -0.228f,  0.772f, 0,
-             1,       1,       1, 1
+       0.0f, 0.0f, 1.0f, 0,
+       0.0f, 1.0f, 0.0f, 0,
+       1.0f, 0.0f, 0.0f, 0,
+          0,    0,    0, 1
     };
 
     public static void applyDaltonizerSetting(Context context, int userId) {
